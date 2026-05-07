96269c637ea0a0cd4c15bbba6a4f193e7bef4a29
Ethantobot <ethantobot7@gmail.com>
2026-05-07 22:43:39 +0200

    Update MobileControlManager.hx

--- source/mobile/MobileControlManager.hx
+++ source/mobile/MobileControlManager.hx
@@ -2,50 +2,48 @@ package mobile;
 
 import flixel.FlxCamera;
 import flixel.FlxG;
+import flixel.group.FlxGroup;
 import flixel.util.FlxDestroyUtil;
-import mobile.MobilePad;
-import mobile.Hitbox;
 import mobile.JoyStick;
-import flixel.FlxBasic;
 
 /**
  * A simple mobile manager for who doesn't want to create these manually
  * if you're making big projects or have a experience to how controls work, create the controls yourself
  */
-class MobileControlManager implements IFlxDestroyable {
-	public var mobilePadCam:FlxCamera;
+class MobileControlManager {
+	public var currentState:Dynamic;
+
 	public var mobilePad:FunkinMobilePad;
+	public var mobilePadCam:FlxCamera;
 	public var joyStickCam:FlxCamera;
-	public var joyStick:FunkinJoyStick;
+	public var joyStick:JoyStick;
 	public var hitboxCam:FlxCamera;
 	public var hitbox:FunkinHitbox;
-	public var curState:Dynamic;
 
-	public function new(target:Dynamic):Void
+	public function new(state:Dynamic):Void
 	{
-		curState = target;
-		//trace("MobileControlManager initialized.");
+		this.currentState = state;
+		trace("MobileControlManager initialized.");
 	}
 
 	//for lua shit
 	public function makeMobilePad(DPad:String, Action:String)
 	{
 		if (mobilePad != null) removeMobilePad();
-		mobilePad = new FunkinMobilePad(DPad, Action);
-		mobilePad.alpha = ClientPrefs.data.mobilePadAlpha;
+		mobilePad = new FunkinMobilePad(DPad, Action, ClientPrefs.data.mobilePadAlpha);
 	}
 
 	public function addMobilePad(DPad:String, Action:String)
 	{
 		makeMobilePad(DPad, Action);
-		curState.add(mobilePad);
+		currentState.add(mobilePad);
 	}
 
 	public function removeMobilePad():Void
 	{
 		if (mobilePad != null)
 		{
-			curState.remove(mobilePad);
+			currentState.remove(mobilePad);
 			mobilePad = FlxDestroyUtil.destroy(mobilePad);
 		}
 
@@ -64,24 +62,21 @@ class MobileControlManager implements IFlxDestroyable {
 		mobilePad.cameras = [mobilePadCam];
 	}
 
-	public function makeHitbox(?mode:String, ?hints:Bool)
-	{
+	public function makeHitbox(?mode:String, ?hints:Bool) {
 		if (hitbox != null) removeHitbox();
-		hitbox = new FunkinHitbox(mode, hints);
-		hitbox.alpha = ClientPrefs.data.hitboxAlpha;
+		hitbox = new FunkinHitbox(mode, hints, ClientPrefs.data.hitboxAlpha);
 	}
 
-	public function addHitbox(?mode:String, ?hints:Bool)
-	{
+	public function addHitbox(?mode:String, ?hints:Bool) {
 		makeHitbox(mode, hints);
-		curState.add(hitbox);
+		currentState.add(hitbox);
 	}
 
 	public function removeHitbox():Void
 	{
 		if (hitbox != null)
 		{
-			curState.remove(hitbox);
+			currentState.remove(hitbox);
 			hitbox = FlxDestroyUtil.destroy(hitbox);
 		}
 
@@ -100,24 +95,23 @@ class MobileControlManager implements IFlxDestroyable {
 		hitbox.cameras = [hitboxCam];
 	}
 
-	public function makeJoyStick(x:Float = 0, y:Float = 0, ?graphic:String, ?onMove:Float->Float->Float->String->Void, size:Float = 1):Void
+	public function makeJoyStick(?stickPath:String, x:Float, y:Float, radius:Float = 0, ease:Float = 0.25, size:Float = 1):Void
 	{
 		if (joyStick != null) removeJoyStick();
-		joyStick = new FunkinJoyStick(x, y, graphic, onMove);
-		joyStick.scale.set(size, size);
+		joyStick = new JoyStick(stickPath, x, y, radius, ease, size);
 	}
 
-	public function addJoyStick(x:Float = 0, y:Float = 0, ?graphic:String, ?onMove:Float->Float->Float->String->Void, size:Float = 1):Void
+	public function addJoyStick(?stickPath:String, x:Float, y:Float, radius:Float = 0, ease:Float = 0.25, size:Float = 1):Void
 	{
-		makeJoyStick(x, y, graphic, onMove, size);
-		curState.add(joyStick);
+		makeJoyStick(stickPath, x, y, radius, ease, size);
+		currentState.add(joyStick);
 	}
 
 	public function removeJoyStick():Void
 	{
 		if (joyStick != null)
 		{
-			curState.remove(joyStick);
+			currentState.remove(joyStick);
 			joyStick = FlxDestroyUtil.destroy(joyStick);
 		}
 
